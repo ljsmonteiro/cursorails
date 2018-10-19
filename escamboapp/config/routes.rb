56c939d8519c46b7faa738e61f0be8c4429604e2
ljsmonteiro <lucio.suruagy@gmail.com>
2018-10-19 18:22:32 +0000

    Primeiro GIT

---
+++ escamboapp/config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  namespace :site do
+    get 'home', to: 'home#index'
+  end
+  
+  get 'backoffice', to: 'backoffice/dashboard#index'
+  
+  namespace :backoffice do
+    get 'dashboard', to: 'dashboard#index'
+  end
+  devise_for :admins
+  devise_for :members
+  
+  root 'site/home#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
